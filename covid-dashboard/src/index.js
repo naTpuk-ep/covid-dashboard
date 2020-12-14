3326f893d182eecbf99d8a4ce43c6959fdce018b
naTpuk-ep <naTpuk.ep@gmail.com>
2020-12-14 18:37:47 +0300

    init: config, data api module

---
+++ covid-dashboard/src/index.js
@@ -0,0 +1,30 @@
+import getData from './js/getData.js';
+import '../autoCloser.js';
+
+const APIUrls = {
+	global: 'https://corona-api.com/timeline', 
+	countries: 'https://corona-api.com/countries',			// <-  push here Api url
+};
+
+
+Object.defineProperty(APIUrls, 'updateFreqSec', { //update Frequency property (in seconds)
+	value: 10,																		//
+	writable: true,
+  enumerable: false,
+  configurable: true,
+});
+
+async function render() {
+	getData(APIUrls)
+		.then(res => {
+			console.log(res);						// ->  pull the data
+		})
+		.catch(e => {
+			console.error(e);
+		});
+}
+
+// localStorage.clear();
+
+render();
+
---
+++ covid-dashboard/webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin  = require('copy-webpack-plugin');
+
+module.exports = {
+
+	devServer: {
+		port: 4000,
+		open: {
+			app: [
+
+				// ---------- custom chrome window ----------------
+
+				'chrome', '--user-data-dir=c:/user_data_dirs/webpack-dev-server-window',
+				// '--disable-extensions-except=,c:/Users/patrick/AppData/Local/Google/Chrome/User Data/Default/Extensions/fmkadmapgofadopljbjfkapdkoienihi/4.10.0_0',
+				'--window-size=710,735', '--window-position=670,0', '--auto-open-devtools-for-tabs', /*'--content-shell-hide-toolbar','--hide-scrollbars',*/  '--new-window'
+			],
+		},
+  },
+
+	mode: 'development',
+
+	entry: {
+		index: './src/index.js'
+	},
+
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'dist')
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, 'src/index.html')
+	}),
+		new CleanWebpackPlugin(),
+		new CopyPlugin({
+      patterns: [
+        { from: "src/assets", to: "assets" },
+      ],
+    }),
+	],
+		
+
+	module: {
+    rules: [
+			{
+        test: /\.s?css$/i,
+        use: [
+          'style-loader', 'css-loader', 'sass-loader',
+        ],
+      },
+			// {
+      //   test: /\.(png|jpe?g|gif|svg)$/i,
+			// 	loader: 'file-loader',
+			// 	options: {
+			// 		name: 'assets/img/[name].[ext]',
+			// 	},
+			// },
+			// {
+      //   test: /\.(ico)$/i,
+			// 	loader: 'file-loader',
+			// 	options: {
+			// 		name: 'assets/[name].[ext]',
+			// 	},
+			// },
+			// {
+      //   test: /\.(mp3)$/i,
+			// 	loader: 'file-loader',
+			// 	options: {
+			// 		name: 'assets/audio/[name].[ext]',
+			// 	},
+      // }
+    ],
+  }
+}
