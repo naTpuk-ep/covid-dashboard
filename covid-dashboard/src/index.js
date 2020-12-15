df6b77a33216893eb9318f8f78856e6c0461bff5
naTpuk-ep <naTpuk.ep@gmail.com>
2020-12-15 19:16:57 +0300

    refacor: structure

--- covid-dashboard/src/index.js
+++ covid-dashboard/src/index.js
@@ -1,6 +1,11 @@
 import './index.scss';
 import getData from './js/getData.js';
 import APIUrls from './js/APIUrls.js';
+import Main from './js/Main';
+import Table from './js/Table';
+import List from './js/List';
+import Maps from './js/Maps';
+import Graph from './js/Graph';
 
 async function render() {
 	getData(APIUrls)
