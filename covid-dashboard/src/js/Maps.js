7e02c6af8bc74af93345e3e60ccff441d230948e
naTpuk-ep <naTpuk.ep@gmail.com>
2020-12-20 09:27:02 +0300

    feat: update-Handler

--- covid-dashboard/src/js/Maps.js
+++ covid-dashboard/src/js/Maps.js
@@ -4,83 +4,149 @@ import countries from './countries';
 const L_Token = 'pk.eyJ1IjoibmF0cHVrLWVwIiwiYSI6ImNraXJxZHZyNzBwejEydGwzcTg3NXpidG4ifQ.GL9jfvvsWibQh0y7c-Ycdw';
 // const geoData = require('../assets/countries.json');
 const geoData = GeoJSON.parse(countries, {Polygon: 'borders'});
-// geoData.type = "FeatureCollection";
-// geoData.features = [];
-// countries.forEach((country, i) => {
-// 	geoData.features[i] = {};
-// 	geoData.features[i].geometry = {};
-// 	geoData.features[i].geometry.coordinates = country.borders;
-// 	// geoData.features[i].geometry.type = "Polygon";
-// 	geoData.features[i].code = country.code;
-// });
-
-// const geoData = countries;
 
 export default class Maps {
 	constructor(data) {
 		this.data = data;
+		this.state = {
+			today: false,
+			sample: false,
+		};
 		const wrapper = document.querySelector('.main__section_map .main__section-content');
 		wrapper.innerHTML = '<div id="map"></div>';
 
-
 		//-----------------
-		var map = L.map('map', {
+		this.map = L.map('map', {
 			worldCopyJump: true,
 			center: [30, 0],
 			zoom: 1.5
 		});
 		L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}`, {
-			attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+			// attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
 			maxZoom: 18,
 			id: 'mapbox/dark-v10',
 			tileSize: 512,
 			zoomOffset: -1,
 			accessToken: L_Token
-		}).addTo(map);
-		// this.getColor = this.getColor.bind(this);
+		}).addTo(this.map);
 		//------------------
+
+		
 		this.geoData = geoData;
 		console.log(this.geoData);
+
 		this.geoData.features.forEach(feature => {
-			feature.getCasesPerMillion = function(data) {
-				let res;
+			feature.getInfoForView = (data) => {
+				let res = 0;
 				data.countries.forEach(country => {
 					if (country.code === feature.properties.code) {
-						res = country.latest_data.calculated.cases_per_million_population;
-						console.log(feature.properties.code, res);
+						res = this.getNeededData(country);
+						// console.log(res);
 					}
 				});
-				return res ? res : 0;
+				return res;
 			};
 		});
+		
 		this.style = this.style.bind(this);
-		this.getColor = this.getColor.bind(this);
-		L.geoJSON(geoData, {style: this.style}).addTo(map);
-		// this.getColor();
+		this.onEachFeature = this.onEachFeature.bind(this);
+		this.highlightHandler = this.highlightHandler.bind(this);
+		this.resetHighlightHandler = this.resetHighlightHandler.bind(this);
+		this.reset = this.reset.bind(this);
+		this.geojson = L.geoJson(this.geoData, {
+			style: this.style,
+			onEachFeature: this.onEachFeature
+		}).addTo(this.map);
+		this.geojson.addTo(this.map);
+		console.log(wrapper);
+		document.querySelector('.main__section_map .main__section-title').addEventListener('click', () => {
+			this.update({
+				today: true,
+				sample: true
+			});
+		});
+		// console.log();
 	}
 
-	
+	getNeededData(country) {
+		const sampleVol = 100000;
+		const population = country.population;
+		console.log(this.state.today);
+		const confirmed = this.state.today ? country.today.confirmed : country.latest_data.confirmed;
+		console.log(country.code, confirmed);
+		return this.state.sample ? sampleVol * confirmed / population : confirmed;
+	}
+
+	update({today, sample}) {
+		console.log(1);
+		// console.log(today, sample);
+		this.state = {
+			today,
+			sample,
+		};
+		console.log(this.state);
+		Object.values(this.geojson._layers).forEach(layer => {
+			this.geojson.resetStyle(layer);
+
+		});
+	}
 
 	getColor(cases) {
-		const casesPerMillion = [];
+		const allCountriesCases = [];
 		this.data.countries.forEach(country => {
-			casesPerMillion.push(country.latest_data.calculated.cases_per_million_population);
+			allCountriesCases.push(this.getNeededData(country));
 		});
-		const max = Math.max(...casesPerMillion);
-		// console.log(cases/max);
-		return `rgb(255 165 0 / ${cases*10/max})`;
-
+		const max = Math.max(...allCountriesCases);
+		const casesVisualFunction = Math.sqrt(1 - ((cases / max) - 1) ** 2);
+		return `rgb(255 165 0 / ${casesVisualFunction})`;
 	}
 
 	style(feature) {
     return {
-        fillColor: this.getColor(feature.getCasesPerMillion(this.data)),
-        weight: 2,
+        fillColor: this.getColor(feature.getInfoForView(this.data)),
+        weight: 1,
         opacity: 1,
         color: 'white',
-        dashArray: '3',
+        // dashArray: '3',
         fillOpacity: 0.7
     };
+	}
+
+	highlightHandler(e) {
+    let layer = e.target;
+
+    layer.setStyle({
+        weight: 3,
+        color: 'white',
+        dashArray: '',
+        fillOpacity: 1
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+	}
+
+	resetHighlightHandler(e) {
+		// console.log(e.target);
+		// console.log(this.geojson);
+    this.geojson.resetStyle(e.target);
+	}
+
+	zoomHandler(e) {
+    this.map.fitBounds(e.target.getBounds());
+	}
+
+	reset(e) {
+		this.geojson.resetStyle(e.target);
+	}
+
+	onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: this.highlightHandler,
+        mouseout: this.resetHighlightHandler,
+        // click: /*this.zoomHandler*/ this.reset,
+    });
 	}
 }
  
